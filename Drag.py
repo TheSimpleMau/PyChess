b9f0780b10b34b8d2b36b6589c738ef9edd6d297
Mauricio Olguin <mauolguisanchez@gmail.com>
2022-11-29 00:12:48 -0600

    Showing only places where can go: OK

--- Drag.py
+++ Drag.py
@@ -2,7 +2,9 @@ from Board import Board
 from PIL import Image, ImageTk
 from time import sleep
 
+
 class Drag(Board):
+
 
     def __init__(self, *args, **kargs) -> None:
         super().__init__(*args, **kargs)
@@ -17,9 +19,10 @@ class Drag(Board):
         self.Bishop = [[],[],[[100,100],[-100,-100],[100,-100],[-100,100]]]
         self.Knight = [[],[],[[200,100],[200,-100],[-200,-100],[-200,100],[100,200],[100,-200],[-100,-200],[-100,200]]]
         self.Rook = [[100,-100],[100,-100],[]]
-        self.Pawn = None #Special case for pawn piece
+        self.Pawn = None #Special case for pawn piece 
         #To move the actual piece
         self.bind("<Button 1>",self.movePiece)
+
 
     def getCenter(self,number):
         '''
@@ -30,13 +33,14 @@ class Drag(Board):
         else:
             return 50
 
+
     def getClickOrigin(self,eventOrigin):
         '''
         This function locates the actual place where the usere click on the board
         '''
         self.x_click = eventOrigin.x
         self.y_click = eventOrigin.y
-        print(self.x_click,self.y_click)
+
 
     def getPieceToMove(self,event):
         '''
@@ -50,9 +54,10 @@ class Drag(Board):
             if x == self.x_click and y == self.y_click:
                 return piece
 
+
     def displayMoves(self,currentPosition:list,moves:list,infinity:bool,idxOfImage:int):
         '''
-        This function show to the user all the places where can move the piece selected
+        This function show to the user all the places where the piece can move
         '''
         # To obtain the current x and y position of the piece
         currentX = currentPosition[0]
@@ -65,31 +70,62 @@ class Drag(Board):
                     rectangleX = self.getCenter(currentX)-50
                     rectangleY = self.getCenter(currentY)-50
                     if x > 0:
+                        notLegalMove = False
                         while currentX+plusX < 800:
-                            self.board.create_rectangle(rectangleX+100,rectangleY,rectangleX+200,rectangleY+100,fill="#476042")
-                            plusX+=int(x)
-                            rectangleX+=100
+                            nextPosition = [currentX+plusX,currentY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX+100,rectangleY,rectangleX+200,rectangleY+100,fill="#476042")
+                                plusX+=int(x)
+                                rectangleX+=100
+                            else:
+                                break
                     else:
+                        notLegalMove = False
                         while currentX+plusX > 0:
-                            self.board.create_rectangle(rectangleX-100,rectangleY,rectangleX,rectangleY+100,fill="#476042")
-                            # self.board.create_image(currentX+plusX,currentY,image=self.images[idxOfImage])
-                            plusX+=int(x)
-                            rectangleX-=100
+                            nextPosition = [currentX+plusX,currentY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX-100,rectangleY,rectangleX,rectangleY+100,fill="#476042")
+                                plusX+=int(x)
+                                rectangleX-=100
+                            else:
+                                break
             if len(moves[1]) != 0:
                 for y in moves[1]:
                     plusY = int(y)
                     rectangleX = self.getCenter(currentX)-50
                     rectangleY = self.getCenter(currentY)-50
                     if y > 0:
+                        notLegalMove = False
                         while currentY+plusY < 800:
-                            self.board.create_rectangle(rectangleX,rectangleY+100,rectangleX+100,rectangleY+200 ,fill="#476042")
-                            plusY+=int(y)
-                            rectangleY+=100
+                            nextPosition = [currentX,currentY+plusY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX,rectangleY+100,rectangleX+100,rectangleY+200 ,fill="#476042")
+                                plusY+=int(y)
+                                rectangleY+=100
+                            else:
+                                break
                     else:
+                        notLegalMove =False
                         while currentY+plusY > 0:
-                            self.board.create_rectangle(rectangleX,rectangleY-100,rectangleX+100,rectangleY,fill="#476042")
-                            rectangleY-=100
-                            plusY+=int(y)
+                            nextPosition = [currentX,currentY+plusY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX,rectangleY-100,rectangleX+100,rectangleY,fill="#476042")
+                                rectangleY-=100
+                                plusY+=int(y)
+                            else:
+                                break
             if len(moves[2]) != 0:
                 for x,y in moves[2]:
                     plusX = x
@@ -97,58 +133,134 @@ class Drag(Board):
                     rectangleX = self.getCenter(currentX)-50
                     rectangleY = self.getCenter(currentY)-50
                     if x>0 and y>0:
+                        notLegalMove = False
                         while (currentX+plusX and currentY+plusY) < 800:
-                            self.board.create_rectangle(rectangleX+100,rectangleY+100,rectangleX+200,rectangleY+200,fill="#476042")
-                            # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
-                            plusX+=x
-                            plusY+=y
-                            rectangleX+=100
-                            rectangleY+=100
+                            nextPosition = [currentX+plusX,currentY+plusY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX+100,rectangleY+100,rectangleX+200,rectangleY+200,fill="#476042")
+                                # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
+                                plusX+=x
+                                plusY+=y
+                                rectangleX+=100
+                                rectangleY+=100
+                            else:
+                                break
                     elif x>0 and y<0:
+                        notLegalMove = False
                         while currentX+plusX < 800 and currentY+plusY > 0:
-                            self.board.create_rectangle(rectangleX+100,rectangleY-100,rectangleX+200,rectangleY,fill="#476042")
-                            # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
-                            plusX+=x
-                            plusY+=y
-                            rectangleX+=100
-                            rectangleY-=100
+                            nextPosition = [currentX+plusX,currentY+plusY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX+100,rectangleY-100,rectangleX+200,rectangleY,fill="#476042")
+                                # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
+                                plusX+=x
+                                plusY+=y
+                                rectangleX+=100
+                                rectangleY-=100
+                            else:
+                                break
                     elif x<0 and y<0:
+                        notLegalMove = False
                         while currentX+plusX > 0 and currentY+plusY > 0:
-                            self.board.create_rectangle(rectangleX-100,rectangleY-100,rectangleX,rectangleY,fill="#476042")
-                            # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
-                            plusX+=x
-                            plusY+=y
-                            rectangleX-=100
-                            rectangleY-=100
+                            nextPosition = [currentX+plusX,currentY+plusY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX-100,rectangleY-100,rectangleX,rectangleY,fill="#476042")
+                                # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
+                                plusX+=x
+                                plusY+=y
+                                rectangleX-=100
+                                rectangleY-=100
+                            else:
+                                break
                     elif x<0 and y>0:
+                        notLegalMove = False
                         while currentX+plusX > 0 and currentY+plusY < 800:
-                            self.board.create_rectangle(rectangleX-100,rectangleY+100,rectangleX,rectangleY+200,fill="#476042")
-                            # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
-                            plusX+=x
-                            plusY+=y
-                            rectangleX-=100
-                            rectangleY+=100
+                            nextPosition = [currentX+plusX,currentY+plusY]
+                            for position in self.initial_images_position:
+                                if nextPosition == position[1]:
+                                    notLegalMove = True
+                            if not notLegalMove:
+                                self.board.create_rectangle(rectangleX-100,rectangleY+100,rectangleX,rectangleY+200,fill="#476042")
+                                # self.board.create_image(currentX+plusX,currentY+plusY,image=self.images[idxOfImage])
+                                plusX+=x
+                                plusY+=y
+                                rectangleX-=100
+                                rectangleY+=100
+                            else:
+                                break
         #If not, then we only iterate to one place
         else:
             if len(moves[0]) != 0:
                 for x in moves[0]:
+                    notLegalMove = False
                     if currentX+x < 800 and currentX+x > 0:
-                        self.board.create_image(currentX+x,currentY,image=self.images[idxOfImage])
+                        rectangleX = self.getCenter(currentX)-50
+                        rectangleY = self.getCenter(currentY)-50
+                        nextPosition = [currentX+x,currentY]
+                        for position in self.initial_images_position:
+                            if nextPosition == position[1]:
+                                notLegalMove = True
+                        if x>0 and not notLegalMove:
+                            self.board.create_rectangle(rectangleX+100,rectangleY,rectangleX+200,rectangleY+100,fill="#476042")
+                        elif x<0 and not notLegalMove:
+                            self.board.create_rectangle(rectangleX-100,rectangleY,rectangleX,rectangleY+100,fill="#476042")
+                        else:
+                            continue
             if len(moves[1]) != 0:
                 for y in moves[1]:
+                    notLegalMove = False
                     if currentY+y < 800 and currentY+y > 0:
-                        self.board.create_image(currentX,currentY+y,image=self.images[idxOfImage])
-            
+                        rectangleX = self.getCenter(currentX)-50
+                        rectangleY = self.getCenter(currentY)-50
+                        nextPosition = [currentX,currentY+y]
+                        for position in self.initial_images_position:
+                            if nextPosition == position[1]:
+                                notLegalMove = True
+                        if y>0 and not notLegalMove:
+                            self.board.create_rectangle(rectangleX,rectangleY+100,rectangleX+100,rectangleY+200,fill="#476042")
+                        elif y<0 and not notLegalMove:
+                            self.board.create_rectangle(rectangleX,rectangleY-100,rectangleX+100,rectangleY,fill="#476042")
+                        else:
+                            continue
             if len(moves[2]) != 0:
                 for x,y in moves[2]:
+                    notLegalMove = False
                     if currentX+x<800 and currentY+y<800:
-                        self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
-                    elif currentX+x<800 and currentY+y>0:
-                        self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
-                    elif currentX+x>0 and currentY+y>0:
-                        self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
-                    elif currentX+x>0 and currentY+y<800:
-                        self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
+                        rectangleX = self.getCenter(currentX)-50
+                        rectangleY = self.getCenter(currentY)-50
+                        nextPosition = [currentX+x,currentY+y]
+                        for position in self.initial_images_position:
+                            if nextPosition == position[1]:
+                                notLegalMove = True
+                        if not notLegalMove:
+                            self.board.create_rectangle(rectangleX+x,rectangleY+y,rectangleX+x+100,rectangleY+y+100,fill="#476042")
+                        else:
+                            continue
+                        # self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
+                    
+                    ######################################################################
+                    #### CHECK IF THIS PART WORKS WHIOUT ALL THE CODE BELOW COMMENTED ####
+                    ######################################################################
+                    # elif currentX+x<800 and currentY+y>0:
+                    #     pass
+                        # self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
+                        # self.board.create_rectangle(rectangleX,rectangleY,rectangleX,rectangleY,fill="#476042")
+                    # elif currentX+x>0 and currentY+y>0:
+                    #     pass
+                        # self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
+                        # self.board.create_rectangle(rectangleX,rectangleY,rectangleX,rectangleY,fill="#476042")
+                    # elif currentX+x>0 and currentY+y<800:
+                    #     pass
+                        # self.board.create_image(currentX+x,currentY+y,image=self.images[idxOfImage])
+                        # self.board.create_rectangle(rectangleX,rectangleY,rectangleX,rectangleY,fill="#476042")
 
 
     def legalMoves(self,event):
